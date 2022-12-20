0ffef9c10af3249ef6ed191647da4f10e05b0132
mymachine707 <mymaxs441@gmail.com>
2022-12-20 10:48:39 +0500

    migration and postgres writed successfuly

--- storage/storage .go
+++ storage/storage .go
@@ -1,22 +1,33 @@
 package storage
 
-import (
-	"mymachine707/protogen/eCommerce"
-)
+import "lms/lms_book_service/protogen/book_service"
 
 // Interfaces ...
 type Interfaces interface {
-	// For Product
-	AddProduct(id string, entity *eCommerce.CreateProductRequest) error
-	GetProductByID(id string) (*eCommerce.GetProductByIDResponse, error)
-	GetProductList(offset, limit int, search string) (*eCommerce.GetProductListResponse, error)
-	UpdateProduct(product *eCommerce.UpdateProductRequest) error
-	DeleteProduct(idStr string) error
+	// For Author service
+	AddAuthor(id string, entity *book_service.CreateAuthorRequest) error
+	GetAuthorByID(id string) (*book_service.Author, error)
+	GetAuthorList(offset, limit int, search string) (resp *book_service.GetAuthorListResponse, err error)
+	DeleteAuthor(idStr string) error
 
-	// For Category
-	AddCategory(id string, entity *eCommerce.CreateCategoryRequest) error
-	GetCategoryByID(id string) (*eCommerce.Category, error)
-	GetCategoryList(offset, limit int, search string) (resp *eCommerce.GetCategoryListResponse, err error)
-	UpdateCategory(category *eCommerce.UpdateCategoryRequest) error
+	// For Book service
+	AddBook(id string, entity *book_service.CreateBookRequest) error
+	GetBookByID(id string) (*book_service.Book, error)
+	GetBookList(offset, limit int, search string) (resp *book_service.GetBookListResponse, err error)
+	DeleteBook(idStr string) error
+	UpdateBook(book *book_service.UpdateBookRequest) error
+
+	// For Category service
+	AddCategory(id string, entity *book_service.CreateCategoryRequest) error
+	GetCategoryByID(id string) (*book_service.Category, error)
+	GetCategoryList(offset, limit int, search string) (resp *book_service.GetCategoryListResponse, err error)
 	DeleteCategory(idStr string) error
+	UpdateCategory(category *book_service.UpdateCategoryRequest) error
+
+	// For Location service
+	AddLocation(id string, entity *book_service.CreateLocationRequest) error
+	GetLocationByID(id string) (*book_service.Location, error)
+	GetLocationList(offset, limit int, search string) (resp *book_service.GetLocationListResponse, err error)
+	DeleteLocation(idStr string) error
+	UpdateLocation(location *book_service.UpdateLocationRequest) error
 }
